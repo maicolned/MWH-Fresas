e833efa5f924202c009f48c8279472fc09005a9d
maicolned <michael.angulo@ieblanquizal.edu.co>
2026-09-02 16:07:55 -0500

    whatweb

--- script.js
+++ script.js
@@ -24,19 +24,48 @@ function seleccionarProducto(producto) {
 
 
 function personalizar() {
-//toppings seleccionados
+
     let toppings = [];
 
-    //Busca los toppings que estén marcados
-    document.querySelectorAll('')
+    document.querySelectorAll('input[type="checkbox"]:checked')
+    .forEach(function(topping) {
+        toppings.push(topping.value);
+    });
 
+    let salsa = document.querySelector('input[name="salsa"]:checked');
+
+    if (toppings.length === 0) {
+        alert("Selecciona al menos un topping");
+        return;
+    }
+
+    if (!salsa) {
+        alert("Selecciona una salsa");
+        return;
+    }
+
+    let mensaje =
+        "🍓 NUEVO PEDIDO - MWH FRESAS\n\n" +
+        "Toppings: " + toppings.join(", ") + "\n" +
+        "Salsa: " + salsa.value + "\n\n" +
+        "Hola, quiero realizar este pedido.";
+
+    let numero = "573238263347";
+
+    let enlace =
+        "https://web.whatsapp.com/send?phone=" +
+        numero +
+        "&text=" +
+        encodeURIComponent(mensaje);
+
+    window.open(enlace, "_blank");
 }
 
     let mensaje = "🍓 TU PEDIDO 🍓\n\n";
 
     mensaje += "Salsa: " + salsa.value + "\n";
 
-    
+
 
     if (toppings.length > 0) {
 
